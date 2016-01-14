e8c87a56b150dea2be9cde93b5a33991e5d5ffff
Alannah Miranda <ahmirand@ucsd.edu>
2016-01-14 10:30:42 -0800

    correcting output file names

--- tf_binding.sh
+++ tf_binding.sh
@@ -16,10 +16,10 @@ cd ~/biom262-hw1
 cat tf.bed | grep --word-regexp 'NFKB' > tf.nfkb.bed
 
 #exercise 2
-awk '{if($3=="transcript"){print $0}}' gencode.v19.annotation.chr22.gtf > gencode.v19.annotation.ch$
+awk '{if($3=="transcript"){print $0}}' gencode.v19.annotation.chr22.gtf > gencode.v19.annotation.chr22.transcript.gtf
 
 #exercise 3
 module load biotools
-bedtools flank -i gencode.v19.annotation.chr22.transcript.gtf -g hg19.genome -l 2000 -r 0 > gencode$
+bedtools flank -i gencode.v19.annotation.chr22.transcript.gtf -g hg19.genome -l 2000 -r 0 > gencode.v19.annotation.chr22.transcript.promoter.gtf
 
-git blame tf.binding
+git blame tf.binding.sh
