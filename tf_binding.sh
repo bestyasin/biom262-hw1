6a2272c441ff13ed28b72d59eb19650a00f581c9
bestyasin <ytorrest@ucsd.edu>
2016-01-20 15:31:10 -0800

    Update tf_binding.sh

--- tf_binding.sh
+++ tf_binding.sh
@@ -17,4 +17,15 @@ awk '{if($3=="transcript"){print $0}}' gencode.v19.annotation.chr22.gtf > gencod
 module load biotools
 bedtools flank -i gencode.v19.annotation.chr22.transcript.gtf -g hg19.genome -l 2000 -r 0 > gencode.v19.annotation.chr22.transcript.promoter.gtf
 
+#exercise 4
+module load biotools
+bedtools intersect -a gencode.v19.annotation.chr22.transcript.promoter.gtf -b tf.nfkb.bed > gencode.v19.annotation.chr22.transcript.promoter.nfkb.gtf
+
+#exercise 5
+# the strand does not matter, whether you take it into account by adding -s or not, the result remains the same.
+module load biotools
+bedtools getfasta -fi GRCh37.p13.chr22.fa -bed gencode.v19.annotation.chr22.transcript.promoter.nfkb.gtf -fo gencode.v19.annotation.chr22.transcript.promoter.nfkb.fasta -s
+
+#exercise 6
+
 git blame tf_binding.sh
