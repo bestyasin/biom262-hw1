b63d9c5c0dadf8b07d8b35b205d62fc19db38a1b
bestyasin <ytorrest@ucsd.edu>
2016-01-20 21:48:50 -0800

    added the changes to the correct branch

--- tf_binding.sh
+++ tf_binding.sh
@@ -17,4 +17,24 @@ awk '{if($3=="transcript"){print $0}}' gencode.v19.annotation.chr22.gtf > gencod
 module load biotools
 bedtools flank -i gencode.v19.annotation.chr22.transcript.gtf -g hg19.genome -l 2000 -r 0 > gencode.v19.annotation.chr22.transcript.promoter.gtf
 
+#exercise 4
+module load biotools
+bedtools intersect -a gencode.v19.annotation.chr22.transcript.promoter.gtf -b t$
+
+#exercise 5
+# the strand does not matter, whether you take it into account by adding -s or $
+module load biotools
+bedtools getfasta -fi GRCh37.p13.chr22.fa -bed gencode.v19.annotation.chr22.tra$
+
+#exercise 6
+
 git blame tf_binding.sh
+
+echo "Hello I am a message in standard error (stderr) >&2 (the >&2 outputs to "$
+
+
+
+
+
+
+
